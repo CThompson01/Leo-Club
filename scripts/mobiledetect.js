99ee45d752bdd58496f5c687d091f108171222e4
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-10-09 13:50:53 -0500

    Fix Mobile Site
    Make mobile site take up 100% of the screen rather than just ending when the text is gone

--- scripts/mobiledetect.js
+++ scripts/mobiledetect.js
@@ -1,1 +1,3 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.text(navigator.userAgent)){document.getElementByTagName("body").setAttribute("style","width:100%");}
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.text(navigator.userAgent)){
+	document.getElementByTagName("body").setAttribute("style","width:100%; height:100%;");
+}
